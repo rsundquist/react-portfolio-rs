e998cfd992f2e836673b381c16db47d60aba81a5
Rebecca Sundquist <rsundquist629@gmail.com>
2022-03-16 12:48:36 -0400

    adding basic file structure

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
+  );
+}
